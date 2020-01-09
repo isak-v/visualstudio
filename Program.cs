844d7d5bab0ec0aae805990f00c9f1cb2b46254a
Isak Valfridsson <isak.valfridsson@learnet.se>
2020-01-09 13:43:40 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace githubexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worfld!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
